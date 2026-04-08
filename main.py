e9339e0885c640d1e57efea2a316e3688af41f81
Duvan Paternina <dpaterninap@cuc.edu.co>
2026-04-08 09:29:06 -0500

    agregando la funcion de agregar productos en una lista

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ class Producto():
         self.lista_productos = []
 
     def agregar_producto(self, producto):
-        pass
+        if producto and producto not in self.lista_productos:
+            self.lista_productos.append(producto)
+            print(f"Producto '{producto.nombre}' agregado a la lista.")
 
+        elif producto in self.lista_productos:
+            print(f"El producto '{producto.nombre}' ya está en la lista.")
+
+        else:
+            print("Producto no válido. No se agregó a la lista.")
+
+        
     def __str__(self):
         return f"{self.nombre} | ${self.precio}"
     
-
 comando = ""
 while True:
     print("""
@@ -27,9 +35,16 @@ while True:
     comando = input("Ingrese el número de la opción que desea ejecutar (o 'salir' para terminar): ")
 
     if comando == "1":
-        pass
+        nombre_producto = input("Ingrese el nombre del producto: ")
+        precio_producto = float(input("Ingrese el precio del producto: "))
+        nuevo_producto = Producto(nombre_producto, precio_producto)
+        nuevo_producto.agregar_producto(nuevo_producto)
+
+        input("Presione Enter para continuar...")
+
     elif comando == "2":
         pass
+
     elif comando == "3":
         pass
     elif comando == "4":
